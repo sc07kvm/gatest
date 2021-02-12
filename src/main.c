ba90407eb0fca21905980056681f5384df80ec6c
k.molodyakov <k.molodyakov@bi.zone>
2021-02-12 20:08:01 +0300

    Fix lint and add memory leak

--- src/main.c
+++ src/main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
-#include <pcre.h>
-#include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <openssl/ssl.h>
 #include <openssl/x509.h>
 #include <openssl/x509v3.h>
+#include <pcre.h>
 
 int main() {
-	const char *error;
-	int erroffset;
-	char *s_re = "123.*123";
-	pcre *re;
+  volatile int *arr;
+  const char *error;
+  int erroffset;
+  char *s_re = "123.*123";
+  volatile pcre *re;
 
-	printf("PCRE version: %s\n", pcre_version());
-	re = pcre_compile(s_re, PCRE_CASELESS, &error, &erroffset, NULL);
-	if(!re) {
-		printf("Failed regex compile %s at offset %d: %s\n", s_re, erroffset, error);
-		return 1;
-	}
-	printf("OpenSSL version: %s\n", OpenSSL_version(OPENSSL_VERSION));
+  printf("PCRE version: %s\n", pcre_version());
+  re = pcre_compile(s_re, PCRE_CASELESS, &error, &erroffset, NULL);
+  if (!re) {
+    printf("Failed regex compile %s at offset %d: %s\n", s_re, erroffset,
+           error);
+    return 1;
+  }
+  printf("OpenSSL version: %s\n", OpenSSL_version(OPENSSL_VERSION));
+  arr = (int *)malloc(sizeof(int) * 100);
+  printf("%p %p\n", re, arr);
 
-	return 0;
+  return 0;
 }
