20d4435579b39ff757da21c324490c00252bd216
k.molodyakov <k.molodyakov@bi.zone>
2021-02-12 21:31:26 +0300

    Remove error

--- src/main.c
+++ src/main.c
@@ -7,22 +7,11 @@
 #include <pcre.h>
 
 int main() {
-  volatile int *arr;
   const char *error;
   int erroffset;
-  char *s_re = "123.*123";
-  volatile pcre *re;
 
   printf("PCRE version: %s\n", pcre_version());
-  re = pcre_compile(s_re, PCRE_CASELESS, &error, &erroffset, NULL);
-  if (!re) {
-    printf("Failed regex compile %s at offset %d: %s\n", s_re, erroffset,
-           error);
-    return 1;
-  }
   printf("OpenSSL version: %s\n", OpenSSL_version(OPENSSL_VERSION));
-  arr = (int *)malloc(sizeof(int) * 100);
-  printf("%p %p\n", re, arr);
 
   return 0;
 }
