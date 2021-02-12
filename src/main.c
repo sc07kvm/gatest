7d78a2784478c16374d57bdfaa2c48fb8621382e
k.molodyakov <k.molodyakov@bi.zone>
2021-02-12 21:43:47 +0300

    Fix lint

--- src/main.c
+++ src/main.c
@@ -7,9 +7,6 @@
 #include <pcre.h>
 
 int main() {
-  const char *error;
-  int erroffset;
-
   printf("PCRE version: %s\n", pcre_version());
   printf("OpenSSL version: %s\n", OpenSSL_version(OPENSSL_VERSION));
 
